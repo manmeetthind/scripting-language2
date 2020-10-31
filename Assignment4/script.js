efc45d1e59a347aa7332cf5958c1954e974d6964
manmeetthind <57304415+manmeetthind@users.noreply.github.com>
2020-10-31 01:37:22 -0400

    Add files via upload

---
+++ Assignment4/script.js
@@ -0,0 +1,16 @@
+$(document).on("dblclick", function(){
+    $('h2').css('background-color', 'blue');
+});
+
+$(document).on("mouseenter", function(){
+    $('h1').css('background-color', 'red');
+});
+
+$(document).on("mouseup", function(){
+    $('h3').css('background-color', 'yellow');
+});
+
+$(document).on("mousedown", function(){
+    $('h3').css('background-color', 'green');
+});
+
